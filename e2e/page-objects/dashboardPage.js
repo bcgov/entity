ae0b30e214666bdf77136083364258ac164b9632
Lane <John.A.M.Lane@gov.bc.ca>
2019-10-09 13:42:05 -0700

    Complete AR flow with AR and Dashboard page objects

--- e2e/page-objects/dashboardPage.js
+++ e2e/page-objects/dashboardPage.js
@@ -2,9 +2,7 @@ var dashboardCommands = {
     verifyTombstone: function (coopObject) {
         return this.waitForElementVisible('@entityName')
                .assert.containsText('@identifier', coopObject.identifier)
-               .assert.containsText('@entityName', coopObject.legal_name)
-               .assert.containsText('@filingHistoryHeader','Recent Filing History (0)')
-               .assert.containsText('@noFilingsMessage','You have no filing history');
+               .assert.containsText('@entityName', coopObject.legal_name);
     },
     verifyAddresses: function (coopObject) {
         return this.assert.containsText('@officeAddressHeader','Office Addresses')
@@ -18,16 +16,16 @@ var dashboardCommands = {
                .assert.containsText('@deliveryLine2', coopObject.delivery.line2)
                .assert.containsText('@deliveryLine3', coopObject.delivery.line3);
     },
-    verifyDirectorCount: function (coopObject) {
+    verifyDirectorCount: function (director_count) {
         return this.assert.containsText('@currentDirectorsHeader','Current Directors')
-               .expect.elements('div.director-info').count.to.equal(coopObject.director_count);
+               .expect.elements('div.director-info').count.to.equal(director_count);
     },
     startArFiling: function () {
         return this.assert.containsText('@toDoListHeader', 'To Do (1)')
                 .click('@fileNowButton')
                 .assert.urlEquals(this.api.globals.launch_url + '/annual-report')
                 .waitForElementVisible('#AR-header', 'Annual Report Page Loaded');
-    }              
+    }            
     
 };
 module.exports={
@@ -40,6 +38,8 @@ module.exports={
         identifier: "dd.incorp-number",
         filingHistoryHeader: "#dashboardArticle > div > div > section:nth-child(2) > header > h2",
         noFilingsMessage: "#dashboardArticle > div > div > section:nth-child(2) > div > div.no-results.v-card.v-card--flat.v-sheet.theme--light > div > div.no-results__title",
+        topFilingInHistoryName: "#dashboardArticle > div > div > section:nth-child(2) > div > ul > li > div.v-expansion-panel__header > div.list-item > div.list-item__title",
+        topFilingInHistoryStatus: "div.v-expansion-panel__header__status",
         officeAddressHeader: "#dashboardArticle > div > aside > section:nth-child(1) > header > h2",
         launchCOAButton: "#btn-standalone-addresses",
         mailingAddressLabel: "#dashboardArticle > div > aside > section:nth-child(1) > div > ul > li:nth-child(1) > div.address > div.list-item__title",
@@ -53,7 +53,10 @@ module.exports={
         currentDirectorsHeader: "#dashboardArticle > div > aside > section:nth-child(2) > header > h2",
         launchCODButton: "#btn-standalone-directors > div > span",
         toDoListHeader: "#dashboardArticle > div > div > section:nth-child(1) > header > h2",
-        fileNowButton: "#dashboardArticle > div > div > section:nth-child(1) > div > ul > li > div.v-expansion-panel__header > div.list-item > div.list-item__actions > button"
-
+        fileNowButton: "#dashboardArticle > div > div > section:nth-child(1) > div > ul > li > div.v-expansion-panel__header > div.list-item > div.list-item__actions > button",
+        resumeDraftButton: "#btn-draft-resume",
+        toDoButtonMoreActionsArrow: "#dashboardArticle > div > div > section:nth-child(1) > div > ul > li > div.v-expansion-panel__header > div.list-item > div.list-item__actions > span > button.actions__more-actions__btn.v-btn.theme--light.primary > div > i",
+        deleteDraftButton: "#btn-delete-draft",
+        confirmDeleteDraftButton: "#app > div.v-dialog__content.v-dialog__content--active > div > div > div.v-card__actions > button:nth-child(2) > div"
     }
 };
