9daf65dd1eed138ac48d50a29682b75d14897fdc
Lane <John.A.M.Lane@gov.bc.ca>
2019-10-08 23:09:51 -0700

    Added appoint director and certify block

--- e2e/tests/coops/ar.js
+++ e2e/tests/coops/ar.js
@@ -1,7 +1,7 @@
 module.exports={
     '@tags': ['coops', 'single'],
 
-'Signin':function (browser){
+'Signin': function (browser) {
     browser
         .url(browser.globals.launch_url)
         .waitForElementVisible('#input-17')
@@ -21,7 +21,7 @@ module.exports={
     dashboard.startArFiling();
 },
 
-'Confirm initial state of Annual Report':function(browser){
+'Confirm initial state of Annual Report': function (browser) {
     ArPage = browser.page.annualReportPage();   
     ArPage.verfifyInitialArState(browser.globals.CP0000977);
     ArPage.checkFee1('Annual Report', '$30.00');
@@ -32,7 +32,7 @@ module.exports={
 
 },
 
-'Edit the Office Addresses': function(browser){
+'Edit the Office Addresses': function (browser) {
     ArPage = browser.page.annualReportPage();
     ArPage.startEditingOfficeAddresses();
     ArPage.fillInAddressField(ArPage.elements.officeDeliveryStreetAddress.selector, '123 test street', browser);
@@ -46,57 +46,29 @@ module.exports={
     ArPage.checkFee2('Change of Registered Office Address','$20.00');
     ArPage.checkTotalFees('$50.00');
     ArPage.assert.visible('@resetOfficeAddressButton');
+    ArPage.assert.containsText('@officeDeliveryLine1', '123 test street');
+    ArPage.assert.containsText('@officeDeliveryLine2', 'Victoria BC V8V 4K9');
+    ArPage.assert.containsText('@officeDeliveryLine3', 'CANADA');
+    ArPage.assert.containsText('@officeMailingLine1', '123 test street');
+    ArPage.assert.containsText('@officeMailingLine2', 'Victoria BC V8V 4K9');
+    ArPage.assert.containsText('@officeMailingLine3', 'CANADA');
 
   },   
 
-// 'Appoint Director':function(browser){
-//     browser
-//         .assert.containsText('#AR-step-3-header','3. Directors')
-//         .waitForElementVisible('#directors > div:nth-child(2) > button > div')
-//         .click('#directors > div:nth-child(2) > button > div')
-//         .waitForElementVisible('#new-director__first-name')
-//         .setValue('#new-director__first-name','first')
-//         .waitForElementVisible('#directors > div.v-card.v-card--flat.v-sheet.theme--light > ul.list.new-director > li > div > div > form > div.form__row.three-column > div.v-input.item.director-initial.v-text-field.v-text-field--box.v-text-field--enclosed.theme--light > div > div.v-input__slot > div > input[type=text]')
-//         .setValue('#directors > div.v-card.v-card--flat.v-sheet.theme--light > ul.list.new-director > li > div > div > form > div.form__row.three-column > div.v-input.item.director-initial.v-text-field.v-text-field--box.v-text-field--enclosed.theme--light > div > div.v-input__slot > div > input[type=text]','initial')
-//         .waitForElementVisible('#directors > div.v-card.v-card--flat.v-sheet.theme--light > ul.list.new-director > li > div > div > form > div.form__row.three-column > div:nth-child(3) > div > div.v-input__slot > div > input[type=text]')
-//         .setValue('#directors > div.v-card.v-card--flat.v-sheet.theme--light > ul.list.new-director > li > div > div > form > div.form__row.three-column > div:nth-child(3) > div > div.v-input__slot > div > input[type=text]','last')
-//         .setValue('#directors > div.v-card.v-card--flat.v-sheet.theme--light > ul.list.new-director > li > div > div > form > div.meta-container__inner > form > div:nth-child(1) > div > div > div.v-input__slot > div > input[type=text]','123 test st')
-//         .setValue('#directors > div.v-card.v-card--flat.v-sheet.theme--light > ul.list.new-director > li > div > div > form > div.meta-container__inner > form > div:nth-child(2) > div > div > div.v-input__slot > div > input[type=text]','321 test st');
+  'Appoint a Director': function (browser) {
+    ArPage = browser.page.annualReportPage();
+    ArPage.appointDirector();
+    ArPage.checkFeeCount(3);
+    ArPage.checkFee3('Change of Director','$20.00');
+    ArPage.checkTotalFees('$70.00');
+  },
 
-//     browser
-//         .useXpath()
-//         .setValue('//*[@id="directors"]/div[3]/ul[1]/li/div/div/form/div[2]/form/div[3]/div[1]/div/div[1]/div/input','victoria');
+  'Certify who filed': function (browser) {
+    ArPage = browser.page.annualReportPage();
+    ArPage.setValue('@certifyLegalName', 'Tester');
+    ArPage.click('@certifyCheckBox');
+  },
 
-//     browser
-//         .useCss()
-//         .click('#directors > div.v-card.v-card--flat.v-sheet.theme--light > ul.list.new-director > li > div > div > form > div.meta-container__inner > form > div.form__row.three-column > div.v-input.item.v-text-field.v-text-field--box.v-text-field--enclosed.v-select.theme--light > div > div.v-input__slot > div.v-select__slot > div.v-select__selections')
-//         .click('#app > div.v-menu__content.theme--light.menuable__content__active > div > div > div:nth-child(1) > a > div > div')
-//         .setValue('#directors > div.v-card.v-card--flat.v-sheet.theme--light > ul.list.new-director > li > div > div > form > div.meta-container__inner > form > div.form__row.three-column > div:nth-child(3) > div > div.v-input__slot > div > input[type=text]','v1v1v1')
-//         .setValue('#directors > div.v-card.v-card--flat.v-sheet.theme--light > ul.list.new-director > li > div > div > form > div.meta-container__inner > form > div:nth-child(4) > div > div > div.v-input__slot > div > input[type=text]','canada')
-//         .setValue('#directors > div.v-card.v-card--flat.v-sheet.theme--light > ul.list.new-director > li > div > div > form > div.meta-container__inner > form > div:nth-child(5) > div > div > div.v-input__slot > div > textarea','optional')
-//         .assert.visible('#directors > div.v-card.v-card--flat.v-sheet.theme--light > ul.list.new-director > li > div > div > form > div.form__row.form__btns > button:nth-child(2) > div')
-//         .click('#directors > div.v-card.v-card--flat.v-sheet.theme--light > ul.list.new-director > li > div > div > form > div.form__row.form__btns > button.form-primary-btn.v-btn.theme--light.primary > div')
-//         .waitForElementVisible('#director-10 > div > label > div > span:nth-child(1) > span')
-//         .assert.containsText('#director-10 > div > label > div > span:nth-child(1) > span','NEW')
-//         .assert.containsText('#director-10 > div > div > div > div.director_dates > div:nth-child(2)','2019-03-27')
-//         .assert.visible('#director-10-change-btn > div > span')
-//         // .click('#director-5-cease-btn > div > span')
-//         // .assert.visible('#director-7-cease-btn > div > span');
-// },
-// 'Certify Page':function(browser){
-//     browser
-//         .assert.containsText('#AR-step-4-header','4. Certify Correct')
-//         .setValue('#certified-by-textfield','test')
-//         .click('#AR-step-4-container > div > div.v-input.v-input--selection-controls.v-input--checkbox.theme--light > div > div.v-input__slot > div > div')
-//         .assert.containsText('#annual-report-container > aside > div > div > div.container.fee-total > div.fee-total__name','Total Fees')
-//     // .assert.containsText('#annual-report-container > aside > div > div > div.container.fee-total > div.fee-total__value > div','$70.00')
-//         .assert.visible('#ar-save-btn')
-//         .assert.visible('#ar-save-resume-btn')
-//         .assert.visible('#ar-cancel-btn')
-//         .assert.visible('#ar-file-pay-btn > div')
-//         //.assert.visible('#app > div.application--wrap > header > div > div > span > button')
-//         .click('#ar-file-pay-btn > div');
-// },
 // 'PayBC': function (browser) {
 //     browser
 //         .waitForElementVisible('#paylistbutton')
