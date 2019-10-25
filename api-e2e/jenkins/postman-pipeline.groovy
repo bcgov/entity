ac49095e545040c57b340cb412e48549e65fad0d
Lane <John.A.M.Lane@gov.bc.ca>
2019-10-25 09:17:11 -0700

    removed another duplicate env variable

--- api-e2e/jenkins/postman-pipeline.groovy
+++ api-e2e/jenkins/postman-pipeline.groovy
@@ -47,7 +47,7 @@ podTemplate(label: py3nodejs_label, name: py3nodejs_label, serviceAccount: 'jenk
             secretEnvVar(key: 'AUTHURL', secretName: "postman-e2e-secret", secretKey: 'authurl'),
             secretEnvVar(key: 'REALM', secretName: "postman-e2e-secret", secretKey: 'realm'),
             secretEnvVar(key: 'PASSWORD', secretName: "postman-e2e-secret", secretKey: 'password'),
-            secretEnvVar(key: 'CLIENT_SECRET', secretName: "postman-e2e-secret", secretKey: 'client_secret'),
+            secretEnvVar(key: 'CLIENT_SECRET', secretName: "postman-e2e-secret", secretKey: 'clientSecret'),
             secretEnvVar(key: 'USERID', secretName: "postman-e2e-secret", secretKey: 'userid'),
             secretEnvVar(key: 'CLIENTID', secretName: "postman-e2e-secret", secretKey: 'clientid'),
             secretEnvVar(key: 'TOKENURL', secretName: "postman-e2e-secret", secretKey: 'tokenUrl'),
@@ -155,14 +155,14 @@ podTemplate(label: py3nodejs_label, name: py3nodejs_label, serviceAccount: 'jenk
 
                             sh """./node_modules/newman/bin/newman.js run ./${name}.postman_collection.json \
                             --global-var ${url_name}=${url} --global-var auth_url=${AUTHURL} --global-var realm=${REALM} \
-                            --global-var password=${PASSWORD} --global-var client_secret=${CLIENT_SECRET} \
+                            --global-var password=${PASSWORD} --global-var clientSecret=${CLIENT_SECRET} \
                             --global-var userid=${USERID} --global-var clientid=${CLIENTID} \
                             --global-var pay-api-base-url=${PAY-API-BASE-URL} --global-var tokenUrl=${TOKENURL} \
                             --global-var userName=${USERNAME} --global-var passCode=${PASSCODE} \
                             --global-var arEventId=${AREVENTID} --global-var arExtraEventId=${AREXTRAEVENTID} \
                             --global-var base_url=${BASE_URL} --global-var business_identifier=${BUSINESS_IDENTIFIER} \
                             --global-var business_name=${BUSINESS_NAME} --global-var business_number=${BUSINESS_NUMBER} \
-                            --global-var clientId=${CLIENTID} --global-var clientSecret=${CLIENTSECRET} \
+                            --global-var clientId=${CLIENTID} \
                             --global-var coaExtraEventId=${COAEXTRAEVENTID} --global-var codExtraEventId=${CODEXTRAEVENTID} \
                             --global-var colin_id=${COLIN_ID} --global-var corp_num=${CORP_NUM} \
                             --global-var defaultGroup=${DEFAULTGROUP} --global-var entity_passcode=${ENTITY_PASSCODE} \
