847cc82820b1256c81d43f45590ed33677499205
riyazuddinsyed <49958950+riyazuddinsyed@users.noreply.github.com>
2020-01-20 22:13:41 -0800

    Update bcscPage.js

--- e2e/page-objects/bcscPage.js
+++ e2e/page-objects/bcscPage.js
@@ -16,7 +16,7 @@ var loginCommands={
        .assert.containsText('@serialCardHeader','Log in with BC Services Card')
        .assert.containsText('@virtualCardHeader','Virtual Card Testing')
        .waitForElementVisible('@bcscCardInput')
-       .setValue('@bcscCardInput',process.env.user_bcsc)
+       .setValue('@bcscCardInput',process.env.User_bcsc)
        .waitForElementVisible('@continueButton')
        .click('@continueButton')
        .assert.urlEquals(this.api.globals.idtest_url+'/identify')
