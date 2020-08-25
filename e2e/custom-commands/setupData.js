a4b9155401f83992000fdd89f32a75deaf21d54f
vysakh-menon-aot <vysakh.menon@aot-technologies.com>
2020-08-25 15:07:51 -0700

    update nightwatch scripts for calling data-reset-tool

--- e2e/custom-commands/setupData.js
+++ e2e/custom-commands/setupData.js
@@ -3,45 +3,50 @@ var events = require('events');
 var request = require('request');
 var fs = require('fs');
 
-function setupData() {
+function setupData () {
     events.EventEmitter.call(this);
 }
 
 util.inherits(setupData, events.EventEmitter);
 
-setupData.prototype.command = function (identifier, callback) {
-    var filename = identifier + '.xls';
-    var path = './lear-data/' + identifier + '.xls';
+setupData.prototype.command = function (identifier, legalType, callback) {
+    var path = './lear-data/' + identifier + '/';
+    var formData = {};
+
+    var files = fs.readdirSync(path);
+    files.forEach(file => {
+        var fileNameWithoutExtension = file.split('.').slice(0, -1).join('.');
+        formData[fileNameWithoutExtension] = {
+            'value': fs.createReadStream(path + file),
+            'options': {
+                'filename': file,
+                'contentType': null
+            }
+        };
+    });
+
     var self = this;
     this.api.perform(function () {
         setTimeout(function () {
             var options = {
                 'method': 'POST',
-                'url': 'https://data-reset-tool-test.pathfinder.gov.bc.ca/api/fixture/import',
+                'url': 'https://data-reset-tool-test.pathfinder.gov.bc.ca/api/fixture/import/' + legalType,
                 'headers': {
                     'Content-Type': 'multipart/form-data'
                 },
-                'formData': {
-                    'file': {
-                        'value': fs.createReadStream(path),
-                        'options': {
-                            'filename': filename,
-                            'contentType': null
-                        }
-                    }
-                }
+                'formData': formData
             };
             request(options, function (error, response) {
                 if (error) {
                     console.error(error);
                     return;
-                } else if (callback){
+                } else if (callback) {
                     var busObject = JSON.parse(response.body);
                     callback(busObject);
-                }             
-                
+                }
+
             });
-                self.emit('complete');
+            self.emit('complete');
         }, 10);
     });
     return this;
